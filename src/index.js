bfa69874221e94fb839fb47f759016c450d0a0f8
alexlbr <alex.lobera@gmail.com>
2020-03-29 22:54:48 +0100

    finish exercises

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const fetch = require("node-fetch");
 const { ApolloServer, gql } = require("apollo-server");
-const { GraphQLDateTime } = require("graphql-iso-date");
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
 
 const trainingMockData = [
   {
@@ -40,50 +50,40 @@ const discountMockData = [
   }
 ];
 
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  type Training {
-    id: ID!
-    title: String!
-    objectives: String!
-    curriculum: String!
-    overview: String
-    discounts: [Discount]
-    startDate: DateTime
+  # Comments in GraphQL are defined with the hash (#) symbol.
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
   }
-
-  type Discount {
-    id: ID!
-    training: Training
-    code: String!
-    discountPercentage: Int!
-    description: String
-  }
-
-  scalar DateTime
-
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
   type Query {
-    trainings: [Training!]
-    training(id: ID!): Training
-    discounts: [Discount!]
-    discount(id: ID!): Discount
+    books: [Book]
   }
 `;
 
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    trainings: () => fetchTrainings(),
-    discounts: () => fetchDiscounts(),
-    training: (_, { id }) => fetchTrainingById(id),
-    discount: (_, { id }) => fetchDiscountById(id)
-  },
-  Discount: {
-    training: parent => fetchTrainingByUrl(parent.training)
-  },
-  Training: {
-    discounts: ({ discounts }) => discounts.map(fetchDiscountByUrl)
-  },
-  DateTime: GraphQLDateTime
+    books: () => books
+  }
 };
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
 
 function fetchTrainings() {
   // More info about the fetch function? https://github.com/bitinn/node-fetch#json
@@ -117,14 +117,3 @@ function fetchDiscountById(id) {
 function fetchDiscountByUrl(url) {
   return fetch(url).then(res => res.json());
 }
-
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
