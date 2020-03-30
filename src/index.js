3522aedeb8a041656e0d43949f58e494e72df94c
alexlbr <alex.lobera@gmail.com>
2020-03-30 09:12:38 +0100

    update fetch helper functions

--- src/index.js
+++ src/index.js
@@ -49,31 +49,33 @@ const resolvers = {
 
 function fetchTrainings() {
   // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch("https://restapi.reactgraphql.academy/v1/trainings/").then(res =>
-    res.json()
-  );
+  return fetch("https://restapi.reactgraphql.academy/v1/trainings/")
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 function fetchTrainingById(id) {
-  return fetch(
-    `https://restapi.reactgraphql.academy/v1/trainings/${id}`
-  ).then(res => res.json());
+  return fetch(`https://restapi.reactgraphql.academy/v1/trainings/${id}`)
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 function fetchDiscounts() {
-  return fetch("https://restapi.reactgraphql.academy/v1/discounts/").then(res =>
-    res.json()
-  );
+  return fetch("https://restapi.reactgraphql.academy/v1/discounts/")
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 function fetchTrainingByUrl(url) {
-  return fetch(url).then(res => res.json());
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 function fetchDiscountById(id) {
-  return fetch(
-    `https://restapi.reactgraphql.academy/v1/discounts/${id}`
-  ).then(res => res.json());
+  return fetch(`https://restapi.reactgraphql.academy/v1/discounts/${id}`)
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 function fetchDiscountByUrl(url) {
