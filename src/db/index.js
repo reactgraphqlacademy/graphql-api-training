35c8280ac83fdbdad0a54821036c9ec1b82e7a0a
alexlbr <alex.lobera@gmail.com>
2020-04-22 11:55:44 +0100

    update README

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 require("./models/training");
 require("./models/discount");
 
-mongoose.connect(process.env.DB_CONNECTION, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSER}.gcp.mongodb.net/LMS`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
